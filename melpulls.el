e04078d2b18f0c8ecc57023f3c14f17bfc25d8c3
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2026-01-01 12:08:25 -0500

    bump copyright year

--- melpulls.el
+++ melpulls.el
@@ -1,6 +1,6 @@
 ;;; melpulls.el --- Elpaca Menu for outstanding MELPA pull requests  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2025  Nicholas Vollmer
+;; Copyright (C) 2022-2026  Nicholas Vollmer
 
 ;; Author:  Nicholas Vollmer
 ;; URL: https://github.com/progfolio/melpulls
